a7a2b260592c723bc8dbcb414ed0e5f6f35c94bc
drewbug <drew@buglione.us>
2021-12-22 19:23:02 +0100

    Segregate customized firmware files

--- backtalk-br2-external/post-image.sh
+++ backtalk-br2-external/post-image.sh
@@ -3,6 +3,6 @@
 set -u
 set -e
 
-echo "root=/dev/mmcblk0p2 rootwait console=tty1" > ${BINARIES_DIR}/rpi-firmware/cmdline.txt
+install -D -m 0644 ${BR2_EXTERNAL_BACKTALK_PATH}/rpi-firmware/cmdline.txt ${BINARIES_DIR}/rpi-firmware/cmdline.txt
 
 support/scripts/genimage.sh -c ${BR2_EXTERNAL_BACKTALK_PATH}/genimage.cfg
